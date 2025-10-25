10b820de419948e8548686a45d5bc7961789ea9a
tsirdo8 <104384778+tsirdo8@users.noreply.github.com>
2025-10-25 14:36:17 +0400

    oAuth fix

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-// middleware/auth.js
+
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -68,7 +68,8 @@ router.post("/signin/user", async (req, res) => {
 // -------------------- CARWASH SIGNUP --------------------
 router.post("/signup/carwash", async (req, res) => {
   try {
-    const { email, name, location, services, workingHours, password } = req.body;
+    const { email, name, location, services, workingHours, password } =
+      req.body;
 
     const exists = await Carwash.findOne({ name });
     if (exists)
@@ -142,11 +143,8 @@ router.get(
       });
 
     const token = generateToken(existUser._id, existUser.role);
-    res.redirect(`http://localhost:3000/sign-in?token=${token}`);
-  }  
+    res.redirect(`http://sng-back.vercel.app/sign-in?token=${token}`);
+  }
 );
-
-
-
 
 export default router;
